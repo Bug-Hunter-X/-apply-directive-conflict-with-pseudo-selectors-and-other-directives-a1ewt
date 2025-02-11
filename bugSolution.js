a29ef51cbbc77dc191a5ff4542ac32678c671c90
BugHunter-X <askzenai@gmail.com>
2025-02-11 05:20:41 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+```javascript
+// Solution: Apply the pseudo-selector class directly instead of using @apply
+<button class="bg-blue-500 hover:bg-blue-700 focus:outline-none">
+  Click Me
+</button>
+```
